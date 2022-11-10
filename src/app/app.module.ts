68feef70ef62443c90e1148b43d6e74245a34a54
AsadullahQureshi <abduljabbarasadullah@gmail.com>
2022-11-10 22:02:56 +0500

    Skeleton ready for todolist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './Mycomponents/todos/todos.component';
+import { NavbarComponent } from './Mycomponents/navbar/navbar.component';
+import { FooterComponent } from './Mycomponents/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
